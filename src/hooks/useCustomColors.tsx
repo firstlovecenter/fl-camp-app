966c955e7d53a66a101b3c63d85743e1f8fe531d
David Dag Vanderpuije <dabick14@gmail.com>
2023-08-25 13:47:37 +0000

    fix: correct light mode colours

--- src/hooks/useCustomColors.tsx
+++ src/hooks/useCustomColors.tsx
@@ -1,10 +1,10 @@
 import { useColorModeValue } from '@chakra-ui/react'
 
 const useCustomColors = () => {
-  const bg = useColorModeValue('#e6e9ef', '#15181f')
+  const bg = useColorModeValue('#bdbec0', '#15181f')
   const brand = useColorModeValue('#188287', '#2ad5dc')
   const cardBackground = useColorModeValue('#fff', '#222836')
-  const cardRegistrationsText = useColorModeValue('#ebe86d', '#ebe86d')
+  const cardRegistrationsText = useColorModeValue('#6d70eb', '#ebe86d')
   const cardPaidRegistrationsText = useColorModeValue('#66c083', '#66c083')
   const cardSubtitle = useColorModeValue('#cbced6', '#cbced6')
 
