20c86af26cfffaf9c074ab35dc8c7c1fb1652faa
David Dag Vanderpuije <dabick14@gmail.com>
2023-08-25 13:34:37 +0000

    fix: fix hardcoded colours in menucard

--- src/hooks/useCustomColors.tsx
+++ src/hooks/useCustomColors.tsx
@@ -4,8 +4,18 @@ const useCustomColors = () => {
   const bg = useColorModeValue('#e6e9ef', '#15181f')
   const brand = useColorModeValue('#188287', '#2ad5dc')
   const cardBackground = useColorModeValue('#fff', '#222836')
+  const cardRegistrationsText = useColorModeValue('#ebe86d', '#ebe86d')
+  const cardPaidRegistrationsText = useColorModeValue('#66c083', '#66c083')
+  const cardSubtitle = useColorModeValue('#cbced6', '#cbced6')
 
-  return { bg, brand, cardBackground }
+  return {
+    bg,
+    brand,
+    cardBackground,
+    cardRegistrationsText,
+    cardPaidRegistrationsText,
+    cardSubtitle,
+  }
 }
 
 export default useCustomColors
