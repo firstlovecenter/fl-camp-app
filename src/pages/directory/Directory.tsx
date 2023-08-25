36b25b50724c558665ee8cd667af6df0e67b8de3
David Dag Vanderpuije <dabick14@gmail.com>
2023-08-25 10:30:19 +0000

    feat: add types file for data from db

--- src/pages/directory/Directory.tsx
+++ src/pages/directory/Directory.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import MenuCard from '../../components/MenuCard'
 import { Container, Heading } from '@chakra-ui/react'
+import { collection, getDocs } from '@firebase/firestore'
+import { db } from 'firebase'
 
 const Directory = () => {
   const menuItems = [{ number: 1, title: 'Earth' }]
+  const queryEarth = async () => {
+    // const firestore = useFirestore()
+    const earthCollection = await getDocs(collection(db, 'countries'))
+
+    const res: { id: string }[] = []
+
+    earthCollection.forEach((item) => {
+      res.push({
+        id: item.id,
+        ...item.data(),
+      })
+    })
+
+    return res
+  }
+
+  const fetchData = async () => {
+    const res = await queryEarth()
+    console.log(res)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
     <Container>
