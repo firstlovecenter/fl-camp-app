df467cd93bc1d47fbc51b39e9514ebec2e34832e
John-Dag Addy <jaedagy@gmail.com>
2023-08-26 23:07:43 +0000

    fix: implement better placeholder handling for directory

---
+++ src/utils/placeholders.ts
@@ -0,0 +1,23 @@
+export const menuItemsPlaceholder = [
+  {
+    name: 'Earth',
+    link: '/continents-by-earth',
+    paidRegistrations: 0,
+    registrations: 0,
+    id: '1',
+  },
+  {
+    name: 'Mars',
+    link: '/continents-by-mars',
+    paidRegistrations: 0,
+    registrations: 0,
+    id: '2',
+  },
+  {
+    name: 'Jupiter',
+    link: '/continents-by-jupiter',
+    paidRegistrations: 0,
+    registrations: 0,
+    id: '3',
+  },
+]
