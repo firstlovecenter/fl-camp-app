028f7fcca1f7b080d4e7257b33f60845aae3296a
David Dag Vanderpuije <dabick14@gmail.com>
2023-08-28 00:32:41 +0000

    feat: add profile page routes to directoryRoutes

--- src/pages/directory/directoryRoutes.ts
+++ src/pages/directory/directoryRoutes.ts
@@ -5,6 +5,10 @@ const Directory = lazy(() => import('./Directory'))
 const ContinentsByEarth = lazy(() => import('./ContinentsByEarth'))
 const CountriesByContinent = lazy(() => import('./CountriesByContinents'))
 const CampusesByCountry = lazy(() => import('./CampusesByCountry'))
+const CampusProfile = lazy(() => import('./profiles/CampusProfile'))
+const CountryProfile = lazy(() => import('./profiles/CountryProfile'))
+const ContinentProfile = lazy(() => import('./profiles/ContinentProfile'))
+const EarthProfile = lazy(() => import('./profiles/EarthProfile'))
 
 export const directoryRoutes: LazyRouteTypes[] = [
   {
@@ -28,6 +32,30 @@ export const directoryRoutes: LazyRouteTypes[] = [
   {
     path: '/campuses-by-country',
     element: CampusesByCountry,
+    placeholder: true,
+    roles: ['all'],
+  },
+  {
+    path: '/campus-profile',
+    element: CampusProfile,
+    placeholder: true,
+    roles: ['all'],
+  },
+  {
+    path: '/country-profile',
+    element: CountryProfile,
+    placeholder: true,
+    roles: ['all'],
+  },
+  {
+    path: '/continent-profile',
+    element: ContinentProfile,
+    placeholder: true,
+    roles: ['all'],
+  },
+  {
+    path: '/earth-profile',
+    element: EarthProfile,
     placeholder: true,
     roles: ['all'],
   },
