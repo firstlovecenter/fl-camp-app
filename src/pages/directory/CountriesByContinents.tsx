66da8ab29c429c37d97246b3a7d4d2529c4c30e6
David Dag Vanderpuije <dabick14@gmail.com>
2023-08-29 11:34:35 +0000

    fix: redo queries on all menu pages to use reactfire

--- src/pages/directory/CountriesByContinents.tsx
+++ src/pages/directory/CountriesByContinents.tsx
@@ -1,72 +1,61 @@
-import React, { useEffect, useState } from 'react'
 import MenuCard from '../../components/MenuCard'
 import { Container, Heading } from '@chakra-ui/react'
-import { CountriesDataItem } from 'utils/MenuDataTypes'
 import { useChurchId } from 'contexts/IdContext'
-import { query, collection, where, getDocs } from '@firebase/firestore'
-import { db } from 'firebase'
+import { query, collection, where, doc } from '@firebase/firestore'
+import {
+  useFirestore,
+  useFirestoreCollectionData,
+  useFirestoreDocData,
+} from 'reactfire'
+import { ApolloWrapper } from '@jaedag/admin-portal-react-core'
 
 const CountriesByContinent = () => {
   const { continentId } = useChurchId()
   const type = 'countries'
 
-  const [menuItems, setMenuItems] = useState<CountriesDataItem[]>([])
-  const menuList = async (type: string) => {
-    const q = query(
-      collection(db, type),
-      where('continentRef', '==', continentId)
-    )
+  const firestore = useFirestore()
 
-    const querySnapshot = await getDocs(q)
+  const ref = doc(firestore, 'continents', continentId)
+  const { data: continent } = useFirestoreDocData(ref)
 
-    const res: {
-      id: string
-      paidRegistrations: number
-      registrations: number
-      name: string
-    }[] = []
+  const countriesCollection = collection(firestore, type)
+  const countriesQuery = query(
+    countriesCollection,
+    where('continentRef', '==', continentId)
+    // orderBy('name', 'asc')
+  )
 
-    querySnapshot.forEach((item) => {
-      res.push({
-        id: item.id,
-        paidRegistrations: item.data().paidRegistrations,
-        registrations: item.data().registrations,
-        name: item.data().name,
-        ...item.data(),
-      })
-    })
+  const { status, data: countries } = useFirestoreCollectionData(
+    countriesQuery,
+    {
+      idField: 'id',
+    }
+  )
 
-    return res
+  const loading = !countries || !continent
+
+  let error = ''
+  if (status === 'error') {
+    error = 'Error'
   }
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await menuList(type)
-        setMenuItems(res)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-
-    fetchData()
-  }, [])
-
   return (
-    <Container>
-      <Heading my={6}>Breakdown by Country</Heading>
-      {menuItems.map((item, index) => (
-        <MenuCard
-          paidRegistrations={item.paidRegistrations}
-          registrations={item.registrations}
-          name={item.name}
-          id={item.id}
-          type={type}
-          key={index}
-          route={'/country-profile'}
-        />
-      ))}
-    </Container>
+    <ApolloWrapper data={countries} loading={loading} error={error}>
+      <Container>
+        <Heading my={6}>Countries in {continent?.name}</Heading>
+        {countries?.map((item, index) => (
+          <MenuCard
+            paidRegistrations={item.paidRegistrations}
+            registrations={item.registrations}
+            name={item.name}
+            id={item.id}
+            type={type}
+            key={index}
+            route={'/country-profile'}
+          />
+        ))}
+      </Container>
+    </ApolloWrapper>
   )
 }
 
