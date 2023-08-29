fb8e0247d291c451b6584c1dd348e3dc8666257b
David Dag Vanderpuije <dabick14@gmail.com>
2023-08-29 10:24:37 +0000

    fix: redo firebase queries on countries profile in reactfire

--- src/pages/directory/profiles/CountryProfile.tsx
+++ src/pages/directory/profiles/CountryProfile.tsx
@@ -2,78 +2,56 @@ import React, { useEffect, useState } from 'react'
 import { Box, Button, Container, Heading, Stack, Text } from '@chakra-ui/react'
 import DetailsCard from '../../../components/DetailsCard'
 import { useNavigate } from 'react-router-dom'
-import { doc, getDoc } from '@firebase/firestore'
-import { db } from 'firebase'
+import { doc } from '@firebase/firestore'
 import { useChurchId } from 'contexts/IdContext'
-import { CountryPageDetails } from 'utils/ProfileDataTypes'
+import { useFirestore, useFirestoreDocData } from 'reactfire'
+import { ApolloWrapper } from '@jaedag/admin-portal-react-core'
 
 const CountryProfile = () => {
   const navigate = useNavigate()
   const { countryId } = useChurchId()
 
-  const [pageDetails, setPageDetails] = useState<CountryPageDetails>({
-    registrations: 0,
-    paidRegistrations: 0,
-    campuses: 0,
-    name: '',
-    id: '',
-  })
+  const firestore = useFirestore()
+  const ref = doc(firestore, 'countries', countryId)
+  const { status, data: country } = useFirestoreDocData(ref)
 
-  const campusDetails = async () => {
-    const docRef = doc(db, 'countries', countryId)
-    const docSnap = await getDoc(docRef)
+  const loading = !country
 
-    if (docSnap.exists()) {
-      const res: CountryPageDetails = {
-        id: docSnap.id,
-        paidRegistrations: docSnap?.data()?.paidRegistrations,
-        registrations: docSnap?.data()?.registrations,
-        name: docSnap?.data()?.name,
-        campuses: 2,
-      }
-      setPageDetails(res)
-    } else {
-      // docSnap.data() will be undefined in this case
-      console.log('No such document!')
-    }
+  let error = ''
+  if (status === 'error') {
+    error = 'Error'
   }
-
-  const fetchData = async () => {
-    await campusDetails()
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
 
   return (
-    <Container>
-      <Box>
-        <Heading mt={6}>{pageDetails.name}</Heading>
-        <Text>Country</Text>
-        <Box my={6}>
-          <DetailsCard
-            number={pageDetails.registrations}
-            title={'Registrations'}
-          />
-          <DetailsCard
-            number={pageDetails.paidRegistrations}
-            title={'Paid Registrations'}
-          />
-          <DetailsCard number={pageDetails.campuses} title={'Campuses'} />
-          <Stack direction="column" spacing={1}>
-            <Button
-              colorScheme="yellow"
-              variant="solid"
-              onClick={() => navigate('/campuses-by-country')}
-              my={3}
-            >
-              View All Campuses
-            </Button>
-          </Stack>
+    <ApolloWrapper data={country} loading={loading} error={error}>
+      <Container>
+        <Box>
+          <Heading mt={6}>{country?.name}</Heading>
+          <Text>Country</Text>
+          <Box my={6}>
+            <DetailsCard
+              number={country?.registrations}
+              title={'Registrations'}
+            />
+            <DetailsCard
+              number={country?.paidRegistrations}
+              title={'Paid Registrations'}
+            />
+            <DetailsCard number={country?.campuses} title={'Campuses'} />
+            <Stack direction="column" spacing={1}>
+              <Button
+                colorScheme="yellow"
+                variant="solid"
+                onClick={() => navigate('/campuses-by-country')}
+                my={3}
+              >
+                View All Campuses
+              </Button>
+            </Stack>
+          </Box>
         </Box>
-      </Box>
-    </Container>
+      </Container>
+    </ApolloWrapper>
   )
 }
 
