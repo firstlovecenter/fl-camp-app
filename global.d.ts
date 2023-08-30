c92e7d8df52e13b7974167db5f481fdd860c674f
Edem Ahadzi <ahadzi.airdem@gmail.com>
2023-08-30 17:20:07 +0300

    refactor: move home page files under home directory

---
+++ global.d.ts
@@ -0,0 +1,10 @@
+interface Camp {
+  name: string
+  type: string
+  date: string
+  roomOption: string
+  role: string
+  registrationStatus?: string
+  paymentStatus?: string
+  campStatus?: string
+}
