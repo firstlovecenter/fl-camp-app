d8c9ce1b21a5a5d3d2f77a3c3ccad9dca7a2d295
Edem Ahadzi <ahadzi.airdem@gmail.com>
2023-08-31 09:32:45 +0300

    refactor: refactor admin home page

--- global.d.ts
+++ global.d.ts
@@ -14,3 +14,16 @@ interface Role {
   role: string
   name: string
 }
+
+interface FetchedCampData {
+  registrationStatus: string
+  paymentStatus: string
+  role: string
+  roomOption: string
+  id: string
+  name: string
+  campLevel: string
+  startDate: string
+  endDate: string
+  campStatus: boolean
+}
