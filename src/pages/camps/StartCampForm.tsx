749e8d1b7656bb712edb4b5094c7829baed4e637
David Dag Vanderpuije <dabick14@gmail.com>
2023-10-05 12:25:29 +0000

    feat: create start a camp form

---
+++ src/pages/camps/StartCampForm.tsx
@@ -0,0 +1,160 @@
+import React from 'react'
+import { Container, Heading, Box, Button } from '@chakra-ui/react'
+import { Input, Select } from '@jaedag/admin-portal-react-core'
+import * as Yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+import { setDoc, doc } from 'firebase/firestore'
+import { useFirestore } from 'reactfire'
+
+const CampLevelOptions = [
+  { key: 'Global', value: 'global' },
+  { key: 'Continent', value: 'continent' },
+  { key: 'Country', value: 'country' },
+  { key: 'Campus', value: 'campus' },
+]
+
+const StartCampForm = () => {
+  const date = new Date('1990-01-01')
+  const navigate = useNavigate()
+  const firestore = useFirestore()
+
+  const initialValues = {
+    campName: '',
+    campLevel: '',
+    campStart: date,
+    campEnd: date,
+    registrationDeadline: date,
+    paymentDeadline: date,
+  }
+
+  const validationSchema = Yup.object({
+    campName: Yup.string().required('Camp name is a required field'),
+    campLevel: Yup.string().required('Camp Level is a required field'),
+    campStart: Yup.date().required('Camp Start Date is a required field'),
+    campEnd: Yup.date()
+      .required('Camp End Date is a required field')
+      .min(new Date(), 'You cannot end a camp before today'),
+    registrationDeadline: Yup.date()
+      .required('Registration Deadline Date is a required field')
+      .min(new Date(), 'Registration deadline cannot be before today'),
+    paymentDeadline: Yup.date()
+      .required('Payment Deadline Date is a required field')
+      .min(new Date(), 'Payment deadline cannot be before today'),
+  })
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<typeof initialValues>({
+    resolver: yupResolver(validationSchema),
+    defaultValues: initialValues,
+  })
+
+  const onSubmit = async (values: typeof initialValues) => {
+    const data = {
+      campName: values?.campName,
+      campLevel: values?.campLevel,
+      campType: values?.campLevel,
+      startDate: values?.campStart.toISOString().slice(0, 10),
+      endDate: values?.campEnd.toISOString().slice(0, 10),
+      registrationDeadline: values?.registrationDeadline
+        .toISOString()
+        .slice(0, 10),
+      paymentDeadline: values?.paymentDeadline.toISOString().slice(0, 10),
+    }
+
+    console.log('data', data)
+    // await setDoc(doc(firestore, 'camps'), data)
+
+    // navigate('/users')
+  }
+
+  return (
+    <Container p={6}>
+      <Heading>Start A Camp</Heading>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Box my={3}>
+          <Input
+            name="campName"
+            label="Camp Name"
+            placeholder="Camp Name"
+            control={control}
+            errors={errors}
+          />
+        </Box>
+
+        <Box my={3}>
+          <Select
+            name="campLevel"
+            placeholder="Camp Level"
+            label="Camp Level"
+            options={CampLevelOptions}
+            control={control}
+            errors={errors}
+          />
+        </Box>
+
+        <Box my={3}>
+          <Input
+            name="campStart"
+            placeholder="Camp Start"
+            label="Camp Start"
+            type="date"
+            control={control}
+            errors={errors}
+          />
+        </Box>
+
+        <Box my={3}>
+          <Input
+            name="campEnd"
+            placeholder="Camp End"
+            label="Camp"
+            type="date"
+            control={control}
+            errors={errors}
+          />
+        </Box>
+
+        <Box my={3}>
+          <Input
+            name="registrationDeadline"
+            placeholder="Registration Deadline"
+            label="Registration Deadline"
+            type="date"
+            control={control}
+            errors={errors}
+          />
+        </Box>
+
+        <Box my={3}>
+          <Input
+            name="paymentDeadline"
+            placeholder="Payment Deadline"
+            label="Payment Deadline"
+            type="date"
+            control={control}
+            errors={errors}
+          />
+        </Box>
+
+        <Button
+          marginTop={10}
+          type="submit"
+          size="lg"
+          width="100%"
+          isLoading={isSubmitting}
+          colorScheme="whatsapp"
+        >
+          Submit
+        </Button>
+      </form>
+    </Container>
+  )
+}
+
+export default StartCampForm
--- src/pages/camps/campRoutes.ts
+++ src/pages/camps/campRoutes.ts
@@ -2,11 +2,18 @@ import { lazy } from 'react'
 import { LazyRouteTypes } from '../../auth/auth-types'
 
 const Camps = lazy(() => import('./Camps'))
+const StartCampForm = lazy(() => import('./StartCampForm'))
 
 export const campRoutes: LazyRouteTypes[] = [
   {
     path: '/camps',
     element: Camps,
+    placeholder: true,
+    roles: ['all'],
+  },
+  {
+    path: '/camps/start-camp',
+    element: StartCampForm,
     placeholder: true,
     roles: ['all'],
   },
