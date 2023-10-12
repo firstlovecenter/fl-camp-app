16c8a39c148ceb682aedce0f24e826ca6e733476
David Dag Vanderpuije <dabick14@gmail.com>
2023-10-12 17:10:27 +0000

    feat: change user search to search on database

--- src/pages/users/UserList.tsx
+++ src/pages/users/UserList.tsx
@@ -10,42 +10,27 @@ import { UserData } from '../../../global'
 const UserList = () => {
   const firestore = useFirestore()
   const navigate = useNavigate()
-  const userCollection = collection(firestore, 'users')
-  const { data: users } = useFirestoreCollectionData(userCollection, {
-    idField: 'id',
-  })
-
-  const loading = !users
-
-  const usersData: UserData[] = []
-
-  users?.forEach((user: any) => {
-    usersData.push(user)
-  })
-
   return (
-    <ApolloWrapper loading={loading} data={users}>
-      <Container px={6}>
-        <Box mt={6}>
-          <Heading>Users</Heading>
-          <Text>All Users</Text>
-        </Box>
-        <Box>
-          <Center>
-            <Button
-              width="100%"
-              my={4}
-              py={7}
-              colorScheme="telegram"
-              onClick={() => navigate('/add-user')}
-            >
-              Add A User
-            </Button>
-          </Center>
-          <UserSearch users={usersData} />
-        </Box>
-      </Container>
-    </ApolloWrapper>
+    <Container px={6}>
+      <Box mt={6}>
+        <Heading>Users</Heading>
+        <Text>All Users</Text>
+      </Box>
+      <Box>
+        <Center>
+          <Button
+            width="100%"
+            my={4}
+            py={7}
+            colorScheme="telegram"
+            onClick={() => navigate('/add-user')}
+          >
+            Add A User
+          </Button>
+        </Center>
+        <UserSearch />
+      </Box>
+    </Container>
   )
 }
 
