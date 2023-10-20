4e219ceb7356ab2ba492d2b653dc02bf7fa03eef
Marvin Asamoah <marvin.asamoah.123@gmail.com>
2023-10-20 15:33:32 +0000

    feat: build sign up page

--- src/auth/SignUp.tsx
+++ src/auth/SignUp.tsx
@@ -3,6 +3,7 @@ import {
   AlertDescription,
   AlertIcon,
   AlertTitle,
+  Box,
   Button,
   Center,
   Container,
@@ -10,10 +11,14 @@ import {
   Link,
   Text,
 } from '@chakra-ui/react'
+import {
+  ImageUpload,
+  Input,
+  PHONE_NUM_REGEX,
+} from '@jaedag/admin-portal-react-core'
 import { useAuth } from 'contexts/AuthContext'
 import * as Yup from 'yup'
 import { useState } from 'react'
-import { Input } from '@jaedag/admin-portal-react-core'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { Link as RouterLink, useNavigate } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
@@ -24,29 +29,53 @@ const SignUp = () => {
   const { signup } = useAuth()
   const navigate = useNavigate()
   const { inputFieldBackground } = useCustomColors()
+  const date = new Date()
 
   const initialValues = {
     email: '',
     password: '',
     passwordConfirm: '',
+    phone: '',
+    dob: date,
+    pictureUrl: '',
+    firstName: '',
+    lastName: '',
   }
 
   const validationSchema = Yup.object({
+    firstName: Yup.string().required('First name is a required field'),
+    lastName: Yup.string().required('Last name is a required field'),
     email: Yup.string().email().required(),
     password: Yup.string().min(6).required(),
     passwordConfirm: Yup.string()
       .oneOf([Yup.ref('password'), undefined], 'Passwords must match')
       .required('Required'),
+    dob: Yup.date()
+      .required('Date of birth is a required field')
+      .max(new Date(), "You can't be born after today"),
+    phone: Yup.string()
+      .matches(
+        PHONE_NUM_REGEX,
+        `Phone Number must start with + and country code (eg. '+233')`
+      )
+      .required('Phone number is a required field'),
+    pictureUrl: Yup.string().required('You must upload a picture'),
   })
 
   const {
     handleSubmit,
     control,
     formState: { errors, isSubmitting },
+    watch,
+    setValue,
   } = useForm<typeof initialValues>({
     resolver: yupResolver(validationSchema),
     defaultValues: initialValues,
   })
+
+  const firstName = watch('firstName')
+  const lastName = watch('lastName')
+  const email = watch('email')
 
   const onSubmit = async (values: typeof initialValues) => {
     try {
@@ -58,8 +87,8 @@ const SignUp = () => {
   }
 
   return (
-    <Container>
-      <Center height="100vh">
+    <Container my="5rem">
+      <Center>
         <Container>
           <Heading textAlign={'center'} marginBottom={7}>
             Sign Up
@@ -79,6 +108,31 @@ const SignUp = () => {
           )}
           <form onSubmit={handleSubmit(onSubmit)}>
             <Input
+              mb={2}
+              fontSize="1rem"
+              height="3.5rem"
+              size="lg"
+              name="firstName"
+              control={control}
+              errors={errors}
+              placeholder="First Name"
+              _placeholder={{ opacity: 1, color: 'whiteAlpha.700' }}
+              backgroundColor={inputFieldBackground}
+            />
+            <Input
+              mb={2}
+              fontSize="1rem"
+              height="3.5rem"
+              size="lg"
+              name="lastName"
+              control={control}
+              errors={errors}
+              placeholder="Last Name"
+              _placeholder={{ opacity: 1, color: 'whiteAlpha.700' }}
+              backgroundColor={inputFieldBackground}
+            />
+            <Input
+              mb={2}
               fontSize="1rem"
               height="3.5rem"
               size="lg"
@@ -90,7 +144,7 @@ const SignUp = () => {
               backgroundColor={inputFieldBackground}
             />
             <Input
-              marginY={2}
+              mb={2}
               size="lg"
               fontSize="1rem"
               type="password"
@@ -104,6 +158,7 @@ const SignUp = () => {
             />
 
             <Input
+              mb={2}
               size="lg"
               fontSize="1rem"
               type="password"
@@ -115,6 +170,50 @@ const SignUp = () => {
               errors={errors}
               backgroundColor={inputFieldBackground}
             />
+            <Input
+              mb={2}
+              name="dob"
+              placeholder="Date of Birth"
+              type="date"
+              size="lg"
+              fontSize="1rem"
+              height="3.5rem"
+              _placeholder={{ opacity: 1, color: 'whiteAlpha.700' }}
+              control={control}
+              errors={errors}
+              backgroundColor={inputFieldBackground}
+            />
+            <Input
+              mb={2}
+              name="phone"
+              placeholder="Eg. +233 241 23 456"
+              size="lg"
+              fontSize="1rem"
+              type="password"
+              height="3.5rem"
+              _placeholder={{ opacity: 1, color: 'whiteAlpha.700' }}
+              control={control}
+              errors={errors}
+              backgroundColor={inputFieldBackground}
+            />
+            <Box overflow="clip" my={6}>
+              <ImageUpload
+                name="pictureUrl"
+                initialValue={initialValues.pictureUrl}
+                uploadPreset="developer-tests"
+                cloudinaryAccount="firstlovecenter"
+                user={{
+                  id: email ?? '',
+                  firstName: firstName ?? '',
+                  lastName: lastName ?? '',
+                }}
+                setValue={setValue}
+                control={control}
+                errors={errors}
+                placeholder="Upload New Picture"
+                aria-describedby="ImageUpload"
+              />
+            </Box>
 
             <Button
               width="100%"
