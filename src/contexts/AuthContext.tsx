a4e22f4887436408bd624585dbca8be9942ced61
Marvin Asamoah <marvin.asamoah.123@gmail.com>
2023-10-21 09:48:51 +0000

    feat: build sign up page

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,3 +1,6 @@
+import { useToast } from '@chakra-ui/react'
+import { doc, setDoc } from 'firebase/firestore'
+import { useFirestore } from 'reactfire'
 import { auth } from 'firebase'
 import {
   User,
@@ -8,6 +11,7 @@ import {
   signOut,
   updateEmail as updateEmailAuth,
   updatePassword as updatePasswordAuth,
+  AuthErrorCodes,
 } from 'firebase/auth'
 import {
   ReactNode,
@@ -17,6 +21,24 @@ import {
   useState,
 } from 'react'
 
+export interface ValueProps {
+  email: string
+  password?: string
+  passwordConfirm?: string
+  phone: string
+  dob: Date
+  pictureUrl: string
+  firstName: string
+  lastName: string
+}
+
+interface CreateDocumentProps {
+  values: ValueProps
+  email: string
+  userCredential: UserCredential
+  addUser: boolean
+}
+
 interface AuthContextType {
   currentUser: User
   signup: (email: string, password: string) => Promise<UserCredential>
@@ -25,6 +47,12 @@ interface AuthContextType {
   resetPassword: (email: string) => Promise<void>
   updateEmail: (email: string) => Promise<void>
   updatePassword: (password: string) => Promise<void>
+  createUserDocument: ({
+    values,
+    email,
+    userCredential,
+    addUser,
+  }: CreateDocumentProps) => void
 }
 
 const AuthContext = createContext<AuthContextType>({
@@ -35,6 +63,7 @@ const AuthContext = createContext<AuthContextType>({
   resetPassword: () => Promise.resolve(),
   updateEmail: () => Promise.resolve(),
   updatePassword: () => Promise.resolve(),
+  createUserDocument: () => null,
 })
 
 // eslint-disable-next-line react-refresh/only-export-components
@@ -49,6 +78,8 @@ export const useAuth = () => {
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [currentUser, setCurrentUser] = useState<User>({} as User)
   const [loading, setLoading] = useState(true)
+  const toast = useToast()
+  const firestore = useFirestore()
 
   const signup = (email: string, password: string) => {
     console.log('here')
@@ -75,6 +106,52 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     return updatePasswordAuth(currentUser, password)
   }
 
+  const createUserDocument = async ({
+    values,
+    email,
+    userCredential,
+    addUser,
+  }: CreateDocumentProps) => {
+    try {
+      if (addUser) {
+        if (userCredential) {
+          await sendPasswordResetEmail(auth, email)
+          console.log('add user')
+        }
+      }
+      const data = {
+        firstName: values?.firstName.toLowerCase(),
+        lastName: values?.lastName.toLowerCase(),
+        email: values?.email,
+        phone: values?.phone,
+        dob: values?.dob.toISOString().slice(0, 10),
+        image_url: values?.pictureUrl,
+      }
+
+      await setDoc(doc(firestore, 'users', values?.email), { ...data })
+
+      toast({
+        title: 'Account created.',
+        description: "We've created your account for you.",
+        status: 'success',
+        duration: 4000,
+        isClosable: true,
+      })
+    } catch (error) {
+      if (AuthErrorCodes.EMAIL_EXISTS) {
+        toast({
+          title: 'Error.',
+          description: 'Email already in use.',
+          status: 'error',
+          duration: 4000,
+          isClosable: true,
+        })
+      } else {
+        console.log('user creation encountered an error', error)
+      }
+    }
+  }
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setCurrentUser(user as User)
@@ -92,6 +169,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     resetPassword,
     updateEmail,
     updatePassword,
+    createUserDocument,
   }
 
   return (
