87abd49b43dde178b1cc6eb339d4a4f91bd94a75
David Dag Vanderpuije <dabick14@gmail.com>
2023-11-01 18:20:04 +0000

    chore: clean up and other minor fixes

--- src/pages/camps/campRoutes.ts
+++ src/pages/camps/campRoutes.ts
@@ -2,14 +2,10 @@ import { lazy } from 'react'
 import { LazyRouteTypes } from '../../auth/auth-types'
 
 const Camps = lazy(() => import('./Camps'))
-<<<<<<< HEAD
 const CampDetails = lazy(() => import('./CampDetails'))
 const AssignAdmin = lazy(() => import('./AssignAdmin'))
 const StartCamp = lazy(() => import('./StartCampForm'))
-=======
-const StartCampForm = lazy(() => import('./StartCampForm'))
 const RegisterMembers = lazy(() => import('./RegisterMembers'))
->>>>>>> 9f4cf98 (fix: change user camp data to object field instead of subcollection)
 
 export const campRoutes: LazyRouteTypes[] = [
   {
@@ -25,7 +21,6 @@ export const campRoutes: LazyRouteTypes[] = [
     roles: ['all'],
   },
   {
-<<<<<<< HEAD
     path: '/camp/assign-camp-admin',
     element: AssignAdmin,
     placeholder: true,
@@ -37,11 +32,10 @@ export const campRoutes: LazyRouteTypes[] = [
     placeholder: true,
     roles: ['globalAdmin'],
   },
-=======
-    path: '/camps/register-members',
+  {
+    path: '/camp/register-members',
     element: RegisterMembers,
     placeholder: true,
     roles: ['all'],
   },
->>>>>>> 9f4cf98 (fix: change user camp data to object field instead of subcollection)
 ]
